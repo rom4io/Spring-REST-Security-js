a7f4376d9c0e175642d41ba51930aebbbd8d6fbe
Rom4io <salyahov@gmail.com>
2022-04-12 09:20:17 +0300

    Заработал LAZY

---
+++ src/main/java/com/example/bootsecurity/service/UserDetailService.java
@@ -0,0 +1,31 @@
+package com.example.bootsecurity.service;
+
+import com.example.bootsecurity.entity.User;
+import com.example.bootsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+}
